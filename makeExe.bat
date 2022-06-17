a85faa02fc9ad5e5f21ae84ea513ee09df381e4a
Roland Rickborn <r_2@gmx.net>
2022-06-17 11:12:26 +0200

    Advanced makeExe script

--- makeExe.bat
+++ makeExe.bat
@@ -1,7 +1,9 @@
+@echo off
 TITLE Build Executable
 set PATH=c:\Python38\;c:\Python38\Lib\site-packages\;c:\Python38\Scripts\;%PATH%
 set PYTHONPATH=c:\Python38\Lib\
 set PYTHONHOME=c:\Python38\
+rmdir /Q /S release
 pyinstaller ^
     --onefile ^
     --noconsole ^
@@ -11,4 +13,9 @@ pyinstaller ^
     --name bridge ^
     --distpath release ^
     bridge.py
+powershell Compress-Archive release\bridge.exe release\bridge.zip
+del release\bridge.exe
+del /F /Q bridge.spec
+rmdir /Q /S __pycache__
+rmdir /Q /S build
 pause
